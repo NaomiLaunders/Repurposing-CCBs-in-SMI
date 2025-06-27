cfd00f9eba2ca352a4600e7e28bc8ed70630abaa
Naomi Launders <naomi.launders@ucl.ac.uk>
2025-06-27 09:13:11 +0100

    Redacted source files

---
+++ Scripts/1aSetUp_redacted.R
@@ -0,0 +1,69 @@
+####Set up project root directories, packages and functions####
+
+#Clear the workspace
+rm(list = ls(all.names = TRUE))
+
+#Pull from Git so we are synched
+
+system("git pull")
+
+####Libraries needed for this project####
+
+#General admin libraries
+library(tableone)
+library(rprojroot)
+library(tidyverse)
+library(lubridate)
+
+#Analysis specific libraries
+
+####Set the directories so that I can use relative links####
+#Note, these should be deleted before GitHub goes public
+
+root.dir = find_rstudio_root_file()
+DataDir = 
+ProjectDir = 
+OldCodeListDir = 
+
+####Project specific functions####
+
+#Read and write from .txt files keeping the key fields as character vectors
+
+ReadGoldProdObs<-function (x) { 
+  read.table(x, header = TRUE, sep = "\t", dec = ".", quote="", fill=TRUE, colClasses=c(prodcode="character", patid="character"))
+}
+
+ReadGoldMedObs<-function (x) { 
+  read.table(x, header = TRUE, sep = "\t", dec = ".", quote="", fill=TRUE, colClasses=c(medcode="character", patid="character"))
+}
+
+ReadAurumMedObs<-function (x) { 
+  read.table(x, header = TRUE, sep = "\t", dec = ".", quote="", fill=TRUE, colClasses=c(medcodeid="character", patid="character"))
+}
+
+ReadAurumProdObs<-function (x) { 
+  read.table(x, header = TRUE, sep = "\t", dec = ".", quote="", fill=TRUE, colClasses=c(prodcodeid="character", patid="character"))
+}
+
+ReadGoldProdCodelist<-function (x) { 
+  read.table(x, header = TRUE, sep = "\t", dec = ".", quote="", fill=TRUE, colClasses=c(prodcode="character"))
+}
+
+ReadGoldMedCodelist<-function (x) { 
+  read.table(x, header = TRUE, sep = "\t", dec = ".", quote="", fill=TRUE, colClasses=c(medcode="character"))
+}
+
+ReadAurumMedCodelist<-function (x) { 
+  read.table(x, header = TRUE, sep = "\t", dec = ".", quote="", fill=TRUE, colClasses=c(MedCodeId="character"))
+}
+
+ReadAurumProdCodelist<-function (x) { 
+  read.table(x, header = TRUE, sep = "\t", dec = ".", quote="", fill=TRUE, colClasses=c(ProdCodeId="character"))
+}
+
+ReadGeneral<-function (x) { 
+  read.table(x, header = TRUE, sep = "\t", dec = ".", quote="", fill=TRUE)
+}
+
+WriteCodeList<-function (x, y){
+  write.table(x, file = y, col.names=TRUE, row.names=FALSE, sep="\t", dec = ".", quote=FALSE)}
---
+++ Scripts/4dAnalysisPackages_redacted.R
@@ -0,0 +1,45 @@
+####Set up project root directories, packages and functions####
+
+#Clear the workspace
+rm(list = ls(all.names = TRUE))
+
+#Pull from Git so we are synched
+
+system("git pull")
+
+####Libraries needed for this project####
+
+#General admin libraries
+library(tableone)
+library(rprojroot)
+library(tidyverse)
+library(lubridate)
+library(mice)
+library(miceadds)
+library(survival)
+library(survminer)
+
+####Set the directories so that I can use relative links####
+#Note, these should be deleted before GitHub goes public
+
+root.dir = find_rstudio_root_file()
+DataDir = 
+ProjectDir = 
+OldCodeListDir = 
+
+####Set confounders, outcomes and time variables####
+#note doesn't include SMI diagnosis as this is added either as a confounder or as an interaction term
+#Doesn't include region as is stratified by practice ID
+
+Confounders<-c("AgeAtAntiHyp","CCByear", "SMITime", "TimeSinceAnyHyp", "AntiHypDose", "gender",
+               "ethnicity", "PriorMHBin", "PriorSHBin", "PriorGPSHBin","FirstPsychTime", 
+               "AllMI", "AllCHF", "AllCerebrovascular", "Systolic", "Diastolic", "BMIVal","SSRI", "TCA", "Other",
+               "AP", "PriorPhysicalBin", "PriorAccidentBin", "PriorGP", "FullIMD")
+
+Adjust<-paste(Confounders, collapse = " + ")
+
+Outcome<-c("MHBin12", "SHBin12", "AllBin12", "MHBin24", "SHBin24", "AllBin24", 
+           "MHBin6", "SHBin6", "AllBin6", "MHBin3", "SHBin3", "AllBin3")
+
+Time<-c("TimeToMH12", "TimeToSH12", "TimeToAll12", "TimeToMH24", "TimeToSH24", "TimeToAll24",
+        "TimeToMH6", "TimeToSH6", "TimeToAll6", "TimeToMH3", "TimeToSH3", "TimeToAll3")
